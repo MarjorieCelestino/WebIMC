efbcfa49a9b237494ba615fc4ba13fb84429b9da
MarjorieCelestino <marjorie_celestino@hotmail.com>
2016-06-15 14:41:49 -0300

    Criação do Projeto
    criado projeto maven e cofigurado arquivo pom.xml

---
+++ MVC/br.com.imc/src/main/java/imc/br/com/imc/App.java
@@ -0,0 +1,13 @@
+package imc.br.com.imc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
